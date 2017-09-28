6794a0edb4e2346deea6c4ead93c646f8d1f8272
Benjamin Stephens <bs@NCC1776.local>
2017-09-27 23:02:17 -0700

    implements sequelize

---
+++ server/db.js
@@ -0,0 +1,14 @@
+const mysql = require('mysql');
+const Sequelize = require('sequelize');
+
+let sequelize = new Sequelize('toyrpg', 'root', '', {
+  host: 'localhost',
+  dialect: 'mysql',
+
+  pool: {
+    max: 5,
+    min: 0,
+    idle: 10000
+  },
+
+});
