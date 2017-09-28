ce34e9deacafb0f7afa9402672b5cdc415730eb6
Benjamin Stephens <bs@NCC1776.local>
2017-09-27 22:31:06 -0700

    skeletons DB

--- server/app.js
+++ server/app.js
@@ -2,7 +2,12 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const app = express();
+const port = 5220;
 
-app.get('/', (res, res) => {
+app.get('/', (req, res) => {
   console.log('Hello World');
+});
+
+app.listen(port, () => {
+  console.log(`listening on port: ${port}`);
 });
